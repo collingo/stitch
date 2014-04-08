22e351b5394a5ac5c5e19c8f34f0f587ca505b6d
Nick Collings <nick@collingo.com>
2014-04-08 16:30:56 +0100

    refactored gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 
@@ -9,28 +10,41 @@ var jsFiles = [
 	'tests/**/*.js'
 ];
 
-gulp.task('default', ['lint', 'test', 'watch']);
-
-gulp.task('lint', function() {
+function Lint() {
     return gulp.src(jsFiles)
         .pipe(jshint({
-        	evil: true
+            evil: true
         }))
-        .pipe(jshint.reporter('default'));
-});
-
-gulp.task('test', ['lint'], function () {
+        .pipe(jshint.reporter('jshint-stylish'));
+}
+function Test(reporter) {
     return gulp.src('tests/**/*.js')
-        .pipe(mocha({reporter: 'spec'}))
-        .on('error', function() {});
-});
+        .pipe(mocha({reporter: reporter}))
+        .on('error', function() {})
+}
 
-gulp.task('tdd', ['lint'], function () {
-    return gulp.src('tests/**/*.js')
-        .pipe(mocha({reporter: 'dot'}))
-        .on('error', function() {});
-});
+////////////////
+// User tasks //
+////////////////
+gulp.task('default', ['lint', 'test', 'watch']);
+
+gulp.task('lint', Lint);
+
+gulp.task('test', ['lint'], Test.bind(this, 'spec'));
+
+gulp.task('tdd', ['lint'], Test.bind(this, 'dot'));
 
 gulp.task("watch", function() {
     return gulp.watch(jsFiles, ['lint', 'tdd']);
 });
+
+//////////////
+// CI tasks //
+//////////////
+gulp.task('ciLint', function() {
+    return Lint().pipe(jshint.reporter('fail'));
+});
+
+gulp.task('ciTest', ['ciLint'], Test.bind(this, 'spec'));
+
+gulp.task('ci', ['ciLint', 'ciTest']);
